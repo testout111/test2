084104daaae36a4d355d68cf381ae6077b1c89f5
freml <2747620181@qq.com>
2022-04-09 16:14:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 rm -rf /opt/caddy/vmess*
 /opt/xray/xray -config /opt/xray/xray_config.json > /dev/null 2>&1 &
-/opt/caddy/caddy run --config /opt/caddy/caddyfile -adapter caddyfile > /dev/null 2>&1 &
+/opt/caddy/caddy run --config /opt/caddy/Caddyfile -adapter caddyfile > /dev/null 2>&1 &
