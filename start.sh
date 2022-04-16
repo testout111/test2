63a275610efd3883fcd9c1d978dfc7632aee2bc9
freml <2747620181@qq.com>
2022-04-16 22:16:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 rm -rf /opt/caddy/vmess* &
 /opt/xray/xray -config /opt/xray/xray_config.json > /dev/null 2>&1 &
+/opt/nezha-agent -s agent.freml.ml:5555 -p 393d9a4f3054dbf210	&
 /opt/caddy/caddy run --config /opt/caddy/Caddyfile -adapter caddyfile
