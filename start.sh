7095896b2b4869238b1a2342e24668a8c6e53d05
releee <2747620181@qq.com>
2022-11-19 01:21:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm -rf /opt/caddy/vmess* &
-cd /opt/caddy/shell-serverstatus-main && ./status.sh
+cd /opt/caddy/shell-serverstatus-main && nohup sh status.sh &
 /opt/caddy/xray-core/xray -config /opt/caddy/xray-core/config.json > /dev/null 2>&1 &
 /opt/caddy/caddy run --config /opt/caddy/Caddyfile -adapter caddyfile
 
